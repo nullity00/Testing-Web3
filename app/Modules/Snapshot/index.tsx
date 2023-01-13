3b50a8c54e7747e358de79069260071f644cb61a
TheReshma <reshmashaik3011@gmail.com>
2023-01-13 23:20:56 +0530

    more styling

--- app/Modules/Snapshot/index.tsx
+++ app/Modules/Snapshot/index.tsx
@@ -35,7 +35,7 @@ export default function SnapShot() {
   const [prop, setProp] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const [title, setTitle] = useState<string>("");
-  const [snapshotSpace, setSnapshotSpace] = useState<string>("dumbo.eth");
+  const [snapshotSpace, setSnapshotSpace] = useState<string>("");
   const { createProposal, castVote, calculateScores } = useSnapshot();
 
   const { loading: isLoading, data } = useQuery(Space, {
@@ -53,80 +53,78 @@ export default function SnapShot() {
         Snapshot is a protocol for creating and voting on proposals. It is a
         decentralized alternative to traditional governance systems.
       </p>
+      <a href="https://demo.snapshot.org">https://demo.snapshot.org</a>
       <div
+        className="horizontal"
         style={{
-          display: "flex",
-          flexDirection: "column",
-          gap: "1rem",
-          width: "50vw",
-          margin: "3rem 0rem",
+          marginTop: "2rem",
+          justifyContent: "space-between",
         }}
       >
-        <label>Snapshot Space</label>
-        <input
-          width={"fit-content"}
-          type="text"
-          value={snapshotSpace}
-          placeholder="dumbo.eth"
-          prefix="https://demo.snapshot.org/#/"
-          onChange={(e) => {
-            setSnapshotSpace(e.target.value);
-          }}
-        />
-        <label>Proposal Title</label>
-        <input
-          width={"fit-content"}
-          type="text"
-          value={title}
-          placeholder="Proposal 1"
-          onChange={(e) => {
-            setTitle(e.target.value);
-          }}
-        />
-        <label>Snapshot proposal </label>
-        <textarea
-          onChange={(e) => {
-            setContent(e.target.value);
-          }}
-        />
-        {snapshotSpace &&
-          !isLoading &&
-          (data?.space?.id ? (
-            <p style={{ color: "palegreen" }}>
-              Snapshot Space - {data?.space?.name}
-            </p>
-          ) : (
-            <p style={{ color: "red" }}>Incorrect URL</p>
-          ))}
+        <div className="horizontal">
+          <div className="vertical">
+            <label>Snapshot Space</label>
+            <input
+              width={"fit-content"}
+              type="text"
+              value={snapshotSpace}
+              placeholder="dumbo.eth"
+              prefix="https://demo.snapshot.org/#/"
+              onChange={(e) => {
+                setSnapshotSpace(e.target.value);
+              }}
+            />
+            {snapshotSpace &&
+              !isLoading &&
+              (data?.space?.id ? (
+                <p style={{ color: "palegreen" }}>
+                  Snapshot Space - {data?.space?.name}
+                </p>
+              ) : (
+                <p style={{ color: "red" }}>Incorrect URL</p>
+              ))}
+          </div>
 
-        <button
-          onClick={async () => {
-            const res: any = await createProposal(
-              snapshotSpace,
-              title,
-              content,
-              data?.space?.network
-            );
-            setProposal(res.id);
-          }}
-          disabled={!snapshotSpace || !title || !content}
-          style={{
-            padding: "0.7rem",
-            cursor: "pointer",
-          }}
-        >
-          Create Proposal
-        </button>
+          <div className="vertical">
+            <label>Proposal Title</label>
+            <input
+              width={"fit-content"}
+              type="text"
+              value={title}
+              placeholder="Proposal 1"
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+            <label>Snapshot proposal </label>
+            <textarea
+              onChange={(e) => {
+                setContent(e.target.value);
+              }}
+            />
+            <button
+              onClick={async () => {
+                const res: any = await createProposal(
+                  snapshotSpace,
+                  title,
+                  content,
+                  data?.space?.network
+                );
+                setProposal(res.id);
+              }}
+              disabled={!snapshotSpace || !title || !content}
+              style={{
+                padding: "0.7rem",
+                cursor: "pointer",
+              }}
+            >
+              Create Proposal
+            </button>
+          </div>
+        </div>
+
         {proposal && (
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "1rem",
-              width: "50vw",
-              margin: "3rem 0rem",
-            }}
-          >
+          <div className="vertical">
             <p>Proposal Created </p>
             <a
               href={`https://demo.snapshot.org/#/${snapshotSpace}/proposal/${proposal}`}
@@ -137,44 +135,48 @@ export default function SnapShot() {
             >
               Go to snapshot
             </a>
-            <button
-              onClick={() => {
-                castVote(snapshotSpace, proposal, 1);
-              }}
-            >
-              YES
-            </button>
-            <button
-              onClick={() => {
-                castVote(snapshotSpace, proposal, 2);
-              }}
-            >
-              NO
-            </button>
+            <div className="horizontal">
+              <button
+                onClick={() => {
+                  castVote(snapshotSpace, proposal, 1);
+                }}
+              >
+                YES
+              </button>
+              <button
+                onClick={() => {
+                  castVote(snapshotSpace, proposal, 2);
+                }}
+              >
+                NO
+              </button>
+            </div>
           </div>
         )}
-        <label>Load any Proposal</label>
-        <input
-          onChange={(e) => {
-            setProp(e.target.value);
-          }}
-        />
-        {proposalData?.proposal?.id && (
-          <div>
-            <p>Title - {proposalData.proposal.title} </p>
-            <p>State - {proposalData.proposal.state} </p>
-            <p>Snapshot - {proposalData.proposal.snapshot} </p>
-            <p>
-              Scores by strategy -{" "}
-              {proposalData.proposal.scores_by_strategy.map(
-                (strategy: string) => {
-                  return <p key={strategy}>{strategy}</p>;
-                }
-              )}{" "}
-            </p>
-            <p>Scores total - {proposalData.proposal.scores_total} </p>
-          </div>
-        )}
+        <div className="vertical">
+          <label>Load any Proposal</label>
+          <input
+            onChange={(e) => {
+              setProp(e.target.value);
+            }}
+          />
+          {proposalData?.proposal?.id && (
+            <div>
+              <p>Title - {proposalData.proposal.title} </p>
+              <p>State - {proposalData.proposal.state} </p>
+              <p>Snapshot - {proposalData.proposal.snapshot} </p>
+              <p>
+                Scores by strategy -{" "}
+                {proposalData.proposal.scores_by_strategy.map(
+                  (strategy: string) => {
+                    return <p key={strategy}>{strategy}</p>;
+                  }
+                )}{" "}
+              </p>
+              <p>Scores total - {proposalData.proposal.scores_total} </p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
